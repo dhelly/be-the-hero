49d4219f44e0b5a6686b3a0e4b9b6889f1295d88
Jaqueline Fernandes <dhelly@gmail.com>
2020-03-24 17:13:04 -0300

    Back-end da Aplicação

---
+++ backend/src/index.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const routes = require('./router');
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+app.use(routes);
+
+app.listen(3333);
+
